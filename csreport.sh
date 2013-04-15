3f01f2b93a2a0e1b667d3dccfe61e128360a4ed5
Robert Bittle <guywithnose@gmail.com>
2013-04-15 10:15:05 -0400

    Separate the full file list generator so that the report can be run on an arbitrary file list

--- csreport.sh
+++ csreport.sh
@@ -1,43 +1,14 @@
 #!/bin/sh
 
-TMPFILE=`mktemp`
-
-find /www/files -type f \
-    \! -name '*.zip' \
-    \! -name '*.jar' \
-    \! -name '*.cab' \
-    \! -name '*.gif' \
-    \! -name '*.png' \
-    \! -name '*.jpg' \
-    \! -name '*.jpeg' \
-    \! -path '*/.git/*' \
-    \! -path '*/www/files/research/*' \
-    \! -path '*/coldfusion/*' \
-    \! -path '*/Zend/*' \
-    \! -path '*/pear/*' \
-    \! -path '*/sitelogs/*' \
-    \! -path '*/Google/*' \
-    \! -path '*/abraham-twitteroauth-76446fa/*' \
-    \! -path '*/library/CAS/*' \
-    \! -path '*/PEAR/*' \
-    \! -path '*/Apache/*' \
-    \! -path '*/CodeCoverage/Report/*' \
-    \! -path '*/about/newsletter/*' \
-    \! -path '*/feeds/include/xsd/STAR/*' \
-    \! -path '*/phpseclib/*' \
-    > $TMPFILE
-
-
-
-filecount=`wc -l $TMPFILE | awk '{print $1}'`
+filecount=`wc -l $1 | awk '{print $1}'`
 TMPFILE2=`mktemp`
-tr -s '\n' '\000' < $TMPFILE > $TMPFILE2
+tr -s '\n' '\000' < $1 > $TMPFILE2
 linecount=`wc -l --files0-from=$TMPFILE2 | tail -n 1 | awk '{print $1}'`
 errtot=0
 wrntot=0
 filcnt=0
 proccnt=0;
-for file in `grep -v "\.min\.js" $TMPFILE`; do
+for file in `grep -v "\.min\.js" $1`; do
     proccnt=$(($proccnt + 1))
     echo "processing $proccnt of $filecount"
     result=`phpcs --standard=DWS --report=summary $file | grep "^A TOTAL"`
@@ -55,5 +26,4 @@ echo "Files with problems: $filcnt"
 echo "Errors: $errtot"
 echo "Warnings: $wrntot"
 
-rm $TMPFILE
 rm $TMPFILE2
---
+++ fullreport.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+workingDir=`dirname $0`
+
+TMPFILE=`mktemp`
+find /www/files -type f | grep -v "/www/files/research/" | grep -v "\.git" | grep -v "\.zip" | grep -v "\.jar" | grep -v "\.cab" | grep -v "/sitelogs/" | grep -v "/coldfusion/" | grep -v "\.gif" | grep -v "\.jpg" | grep -v "\.png" | grep -v "\.jpeg" > $TMPFILE
+
+$workingDir/csreport.sh $TMPFILE
+
+rm $TMPFILE
