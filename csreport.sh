d0da17b023c364f6e46a5b78a181bcc7d569c577
Robert Bittle <guywithnose@gmail.com>
2013-04-16 12:15:23 -0400

    Add parameters to csreport

--- csreport.sh
+++ csreport.sh
@@ -1,16 +1,43 @@
 #!/bin/sh
 
-filecount=`wc -l $1 | awk '{print $1}'`
+QUIET=0
+SHORT=0
+FILELIST=
+while getopts "qsf:" opt; do
+    case "$opt" in
+    q)
+        QUIET=1
+        ;;
+    s)
+        SHORT=1
+        ;;
+    f)
+        FILELIST=$OPTARG
+        ;;
+    esac
+done
+
+if [ "$FILELIST" = '' ]
+then
+  #Read from STDIN
+  FILELIST=`mktemp`
+  cp /proc/${$}/fd/0 $FILELIST
+fi
+
+filecount=`wc -l $FILELIST | awk '{print $1}'`
 TMPFILE2=`mktemp`
-tr -s '\n' '\000' < $1 > $TMPFILE2
+tr -s '\n' '\000' < $FILELIST > $TMPFILE2
 linecount=`wc -l --files0-from=$TMPFILE2 | tail -n 1 | awk '{print $1}'`
 errtot=0
 wrntot=0
 filcnt=0
 proccnt=0;
-for file in `grep -v "\.min\.js" $1`; do
+for file in `grep -v "\.min\.js" $FILELIST`; do
     proccnt=$(($proccnt + 1))
-    echo "processing $proccnt of $filecount"
+    if [ $QUIET = 0 ]
+    then
+      echo "processing $proccnt of $filecount"
+    fi
     result=`phpcs --standard=DWS --report=summary $file | grep "^A TOTAL"`
     if [ ! -z "$result" ]; then
         errcnt=`echo $result | awk '{print $4}'`
@@ -20,10 +47,15 @@ for file in `grep -v "\.min\.js" $1`; do
         wrntot=`expr $wrncnt + $wrntot`
     fi
 done
-echo "Total Files: $filecount"
-echo "Total Lines: $linecount"
-echo "Files with problems: $filcnt"
-echo "Errors: $errtot"
-echo "Warnings: $wrntot"
+if [ $SHORT = 0 ]
+then
+  echo "Total Files: $filecount"
+  echo "Total Lines: $linecount"
+  echo "Files with problems: $filcnt"
+  echo "Errors: $errtot"
+  echo "Warnings: $wrntot"
+else
+  echo "$filecount|$linecount|$filcnt|$errtot|$wrntot"
+fi
 
 rm $TMPFILE2
--- fullreport.sh
+++ fullreport.sh
@@ -5,6 +5,6 @@ workingDir=`dirname $0`
 TMPFILE=`mktemp`
 find /www/files -type f | grep -v "/www/files/research/" | grep -v "\.git" | grep -v "\.zip" | grep -v "\.jar" | grep -v "\.cab" | grep -v "/sitelogs/" | grep -v "/coldfusion/" | grep -v "\.gif" | grep -v "\.jpg" | grep -v "\.png" | grep -v "\.jpeg" > $TMPFILE
 
-$workingDir/csreport.sh $TMPFILE
+$workingDir/csreport.sh -f $TMPFILE
 
 rm $TMPFILE
