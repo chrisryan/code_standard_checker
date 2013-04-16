14c587cad0102c003ddd2b19771dd1e136697dcf
Robert Bittle <guywithnose@gmail.com>
2013-04-16 13:11:20 -0400

    Add mergeReport

--- csreport.sh
+++ csreport.sh
@@ -24,19 +24,28 @@ then
   cp /proc/${$}/fd/0 $FILELIST
 fi
 
-filecount=`wc -l $FILELIST | awk '{print $1}'`
+FILTEREDLIST=`mktemp`
+for fileName in $(cat $FILELIST | grep -v "\.git" | grep -v "\.zip" | grep -v "\.jar" | grep -v "\.cab" | grep -v "\.gif" | grep -v "\.jpg" | grep -v "\.png" | grep -v "\.jpeg")
+do
+  if [ -f $fileName ]
+  then
+    echo $fileName >> $FILTEREDLIST
+  fi
+done
+
+filecount=`wc -l $FILTEREDLIST | awk '{print $1}'`
 TMPFILE2=`mktemp`
-tr -s '\n' '\000' < $FILELIST > $TMPFILE2
+tr -s '\n' '\000' < $FILTEREDLIST > $TMPFILE2
 linecount=`wc -l --files0-from=$TMPFILE2 | tail -n 1 | awk '{print $1}'`
 errtot=0
 wrntot=0
 filcnt=0
 proccnt=0;
-for file in `grep -v "\.min\.js" $FILELIST`; do
+for file in `grep -v "\.min\.js" $FILTEREDLIST`; do
     proccnt=$(($proccnt + 1))
     if [ $QUIET = 0 ]
     then
-      echo "processing $proccnt of $filecount"
+      echo "processing $proccnt of $filecount" 1>&2
     fi
     result=`phpcs --standard=DWS --report=summary $file | grep "^A TOTAL"`
     if [ ! -z "$result" ]; then
@@ -55,7 +64,7 @@ then
   echo "Errors: $errtot"
   echo "Warnings: $wrntot"
 else
-  echo "$filecount|$linecount|$filcnt|$errtot|$wrntot"
+  echo "$filecount $linecount $filcnt $errtot $wrntot"
 fi
 
 rm $TMPFILE2
--- fullreport.sh
+++ fullreport.sh
@@ -3,7 +3,7 @@
 workingDir=`dirname $0`
 
 TMPFILE=`mktemp`
-find /www/files -type f | grep -v "/www/files/research/" | grep -v "\.git" | grep -v "\.zip" | grep -v "\.jar" | grep -v "\.cab" | grep -v "/sitelogs/" | grep -v "/coldfusion/" | grep -v "\.gif" | grep -v "\.jpg" | grep -v "\.png" | grep -v "\.jpeg" > $TMPFILE
+find /www/files -type f | grep -v "/www/files/research/" | grep -v "/sitelogs/" | grep -v "/coldfusion/" > $TMPFILE
 
 $workingDir/csreport.sh -f $TMPFILE
 
