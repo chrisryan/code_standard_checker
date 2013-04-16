c908940f67279c8cd57fcc51f05561d57c75c916
Robert Bittle <guywithnose@gmail.com>
2013-04-16 13:17:16 -0400

    Add in exclude paths

--- fullreport.sh
+++ fullreport.sh
@@ -3,7 +3,31 @@
 workingDir=`dirname $0`
 
 TMPFILE=`mktemp`
-find /www/files -type f | grep -v "/www/files/research/" | grep -v "/sitelogs/" | grep -v "/coldfusion/" > $TMPFILE
+
+find /www/files -type f \
+    \! -name '*.zip' \
+    \! -name '*.jar' \
+    \! -name '*.cab' \
+    \! -name '*.gif' \
+    \! -name '*.png' \
+    \! -name '*.jpg' \
+    \! -name '*.jpeg' \
+    \! -path '*/.git/*' \
+    \! -path '*/www/files/research/*' \
+    \! -path '*/coldfusion/*' \
+    \! -path '*/Zend/*' \
+    \! -path '*/pear/*' \
+    \! -path '*/sitelogs/*' \
+    \! -path '*/Google/*' \
+    \! -path '*/abraham-twitteroauth-76446fa/*' \
+    \! -path '*/library/CAS/*' \
+    \! -path '*/PEAR/*' \
+    \! -path '*/Apache/*' \
+    \! -path '*/CodeCoverage/Report/*' \
+    \! -path '*/about/newsletter/*' \
+    \! -path '*/feeds/include/xsd/STAR/*' \
+    \! -path '*/phpseclib/*' \
+    > $TMPFILE
 
 $workingDir/csreport.sh -f $TMPFILE
 
