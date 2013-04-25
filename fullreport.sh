6347008c509ccf2a978b3d5441798b8826dc1350
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-04-25 14:08:14 -0400

    Split out find command into a separate command.

--- fullreport.sh
+++ fullreport.sh
@@ -4,32 +4,7 @@ workingDir=`dirname $0`
 
 TMPFILE=`mktemp`
 
-find /www/files -type f \
-    \! -name '*.zip' \
-    \! -name '*.jar' \
-    \! -name '*.cab' \
-    \! -name '*.gif' \
-    \! -name '*.png' \
-    \! -name '*.jpg' \
-    \! -name '*.jpeg' \
-    \! -path '*/.git/*' \
-    \! -path '*/www/files/research/*' \
-    \! -path '*/coldfusion/*' \
-    \! -path '*/Zend/*' \
-    \! -path '*/pear/*' \
-    \! -path '*/sitelogs/*' \
-    \! -path '*/Google/*' \
-    \! -path '*/abraham-twitteroauth-76446fa/*' \
-    \! -path '*/library/CAS/*' \
-    \! -path '*/PEAR/*' \
-    \! -path '*/Apache/*' \
-    \! -path '*/CodeCoverage/Report/*' \
-    \! -path '*/about/newsletter/*' \
-    \! -path '*/feeds/include/xsd/STAR/*' \
-    \! -path '*/phpseclib/*' \
-    \! -path '*/plupload/*' \
-    > $TMPFILE
-
+$workingDir/getFiles.sh /www/files > $TMPFILE
 $workingDir/csreport.sh -f $TMPFILE
 
 rm $TMPFILE
---
+++ getFiles.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+workingDir=`dirname $0`
+
+if [ $# -ne 1 ]; then
+    echo "Usage: `basename $0` codeDirectory" >&2
+    exit 1;
+fi
+
+codeDir=$1
+
+find $codeDir -type f \
+    \! -name '*.zip' \
+    \! -name '*.jar' \
+    \! -name '*.cab' \
+    \! -name '*.gif' \
+    \! -name '*.png' \
+    \! -name '*.jpg' \
+    \! -name '*.jpeg' \
+    \! -path '*/.git/*' \
+    \! -path '*/www/files/research/*' \
+    \! -path '*/coldfusion/*' \
+    \! -path '*/Zend/*' \
+    \! -path '*/pear/*' \
+    \! -path '*/sitelogs/*' \
+    \! -path '*/Google/*' \
+    \! -path '*/abraham-twitteroauth-76446fa/*' \
+    \! -path '*/library/CAS/*' \
+    \! -path '*/PEAR/*' \
+    \! -path '*/Apache/*' \
+    \! -path '*/CodeCoverage/Report/*' \
+    \! -path '*/about/newsletter/*' \
+    \! -path '*/feeds/include/xsd/STAR/*' \
+    \! -path '*/phpseclib/*' \
+    \! -path '*/plupload/*'
