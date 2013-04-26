8797eeaa57b34ade0cec6d049249697e703b4a10
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-04-26 07:16:12 -0400

    Remove TMPFILE2 and count lines in the loop.

--- csreport.sh
+++ csreport.sh
@@ -20,18 +20,17 @@ done
 files=$(cat "${FILELIST}")
 filecount=$(echo "${files}" | wc -l)
 
-TMPFILE2=$(mktemp)
-echo "${files}" | tr -s '\n' '\000' > "${TMPFILE2}"
-linecount=$(wc -l --files0-from="${TMPFILE2}" | tail -n 1 | awk '{print $1}')
-[ -z "${linecount}" ] && linecount="0"
-
 errtot=0
 wrntot=0
 filcnt=0
 proccnt=0
+linecount=0
 while read file; do
     proccnt=$(expr "${proccnt}" + 1)
     "${QUIET}" || echo "processing ${proccnt} of ${filecount} : ${file}" >&2
+
+    lncnt=$(wc -l < "${file}")
+    linecount=$(expr "${linecount}" + "${lncnt}")
 
     if ! phpcs=$(phpcs --standard=DWS --report=summary "${file}"); then
         counts=$(echo "${phpcs}" | sed '/A TOTAL OF/!d; s/A TOTAL OF \([0-9]\+\) ERROR(S) AND \([0-9]\+\) WARNING(S) .*/\1 \2/')
@@ -52,5 +51,3 @@ else
   echo "Errors: ${errtot}"
   echo "Warnings: ${wrntot}"
 fi
-
-rm "${TMPFILE2}"
